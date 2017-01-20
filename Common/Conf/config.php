f58bd0a6693408800e3175131c4989d7b84c5ca4
graduatezhu <graduatezhu@163.com>
2017-01-20 16:18:44 +0800

    更改配置项

--- Common/Conf/config.php
+++ Common/Conf/config.php
@@ -10,7 +10,7 @@ $config = array(
      * 2:REWRITE模式(PATHINFO模式基础上隐藏index.php)
      * 3:兼容模式(普通模式和PATHINFO模式, 可以支持任何的运行环境, 如果你的环境不支持PATHINFO 请设置为3)
      */
-    'URL_MODEL'         => 2, // 如果你的环境不支持PATHINFO 请设置为3
+    'URL_MODEL'         => 2, // 如果环境不支持PATHINFO 请设置为3
     
     'URL_CASE_INSENSITIVE' =>true, //URL忽略大小写
 );
--- Common/Conf/dbconfig.php
+++ Common/Conf/dbconfig.php
@@ -1,20 +1,21 @@
 <?php
 //数据库配置
 $arr1 = array(
-    'DB_TYPE'           => 'mysql',
-    'DB_HOST'           => '127.0.0.1',
-    'DB_NAME'           => '',
-    'DB_USER'           => '',
-    'DB_PWD'            => '',
-    'DB_PORT'           => '3306',
-    'DB_PREFIX'         => 'e_',
+    'DB_TYPE'           => 'mysql',     // 数据库类型
+    'DB_HOST'           => '127.0.0.1', // 服务器地址
+    'DB_NAME'           => '',          // 数据库名
+    'DB_USER'           => '',          // 用户名
+    'DB_PWD'            => '',          // 密码
+    'DB_PORT'           => '3306',      // 端口
+    'DB_PREFIX'         => '',          // 表前缀
+    'DB_CHARSET'        => 'utf8',      // 字符集
 );
 
 $arr2 = array(
 	//File
-	'DATA_CACHE_TYPE'                   => 'File',
-	'DATA_CACHE_TIME'                   => 5,
-	'DATA_CACHE_PREFIX'                   => 'e_',
+	'DATA_CACHE_TYPE'                   => 'File', // 数据缓存类型
+	'DATA_CACHE_TIME'                   => 5,      // 数据缓存有效期 0表示永久缓存
+	'DATA_CACHE_PREFIX'                   => '',   // 缓存前缀
 );
 
 return array_merge($arr1,$arr2);//数据库与应用同在本机
