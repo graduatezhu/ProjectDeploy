e7bdda731bd4a32eb623251c54d3b4327bd131cd
graduatezhu <graduatezhu@163.com>
2017-02-04 11:11:05 +0800

    update admin

--- Application/Admin/Controller/DemoController.class.php
+++ Application/Admin/Controller/DemoController.class.php
@@ -10,6 +10,9 @@ class DemoController extends CommonController {
 	public function _initialize() {
 		parent::_initialize(); // check users authority
 		
+		/*实例化模型对象*/
+		$this->tblChargTmp=D('ChargeTmp');
+		
 	}
 	
 	/**
@@ -20,7 +23,16 @@ class DemoController extends CommonController {
 	 * @throws
 	 */
     public function index() {
-        $data['data']='this is the admin demo project';
-        echo json_encode($data,JSON_UNESCAPED_UNICODE);
+        
+        /*查询数据*/
+        $map['id']=1;
+        $field='id,pile_no,site_no';
+        $list=$this->tblChargTmp->selData($map,0,$field);
+        
+        $tip='Admin demo page';
+
+        $this->assign('list', $list);
+        $this->assign('tip', $tip);
+        $this->display();
     }
 }
